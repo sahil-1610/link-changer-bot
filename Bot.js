b0dd50c2c3849eb95decabdd2537eefcdf908e84
sahil-1610 <sahilkumartiwari.120372@marwadiuniversity.ac.in>
2024-05-27 07:17:46 +0530

    updating bot

--- Bot.js
+++ Bot.js
@@ -15,37 +15,14 @@ function createBot(token) {
     // Check if the message contains a link
     if (msg.text && regex.test(msg.text)) {
         const chatId = msg.chat.id;
-        const modifiedUrl1 = `https://api-sarkari.koyeb.app/pwhls?link=https://d1d34p8vz63oiq.cloudfront.net/${msg.text.match(regex)[1]}/master.m3u8`;
-        const modifiedUrl2 = `https://pwjarvis.onrender.com?v=${msg.text.match(regex)[1]}&quality=`;
-        const modifiedUrl3 = `https://psitoffers.store/1dm.php?vid=${msg.text.match(regex)[1]}`;
-        const modifiedUrl4 = `https://psitoffers.store/testkey.php?vid=${msg.text.match(regex)[1]}&quality=`;
-
+        const modifiedUrl1 = `https://penpencilvod.pc.cdn.bitgravity.com/${msg.text.match(regex)[1]}/master.m3u8`;
+        
         bot.sendMessage(chatId, 
             ` 
-USE THIS LINK ON 1DM APP:
-    
-${modifiedUrl1},
-
-${modifiedUrl3}
-
-USE THIS LINK ON BOT:
- 
-${modifiedUrl2+240}, 
+USE THIS LINK ON 1DM APP AND BOT:
+     
+${modifiedUrl1}
   
-${modifiedUrl2+360}, 
-  
-${modifiedUrl2+480}, 
-  
-${modifiedUrl2+720}
- &
-${modifiedUrl4+240},
-
-${modifiedUrl4+360}, 
-  
-${modifiedUrl4+480}, 
-
-${modifiedUrl4+720}
-
 powered by @omjibotz
             `
         );
