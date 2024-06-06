f2663185c2f3e5f921f2210f6a0ea1a1a8df9b23
sahil-1610 <sahilkumartiwari.120372@marwadiuniversity.ac.in>
2024-06-06 10:58:38 +0530

    updated

--- Bot.js
+++ Bot.js
@@ -1,6 +1,16 @@
 const TelegramBot = require('node-telegram-bot-api');
+
+// Define the base URL globally
+const BASE_URL = 'https://pw-signed-url-26260d62e264.herokuapp.com?v=';
+const PIXEL1 = "&quality=240"
+const PIXEL2 = "&quality=360"
+const PIXEL3 = "&quality=480"
+const PIXEL4 = "&quality=720"
+
+// Define regex patterns
 const regexCloudfront = /cloudfront\.net\s*\/\s*(.*?)\//;
 const regexPwLive = /(?<=pw\.live\/)[^\/]+/;
+const regexVParam = /(?<=v=)[^&?]+/;
 
 // Create a bot instance
 function createBot(token) {
@@ -14,39 +24,47 @@ function createBot(token) {
     bot.on('message', (msg) => {
         const chatId = msg.chat.id;
 
-        // Check if the message contains a link matching the cloudfront pattern
-        if (msg.text && regexCloudfront.test(msg.text)) {
-            const match = msg.text.match(regexCloudfront);
-            const modifiedUrl1 = `https://penpencilvod.pc.cdn.bitgravity.com/${match[1]}/master.m3u8`;
-            
-            bot.sendMessage(chatId, 
-                ` 
+        if (msg.text) {
+            let match;
+            let modifiedUrl;
+
+            // Check if the message contains a link matching the cloudfront pattern
+            if (regexCloudfront.test(msg.text)) {
+                match = msg.text.match(regexCloudfront);
+                modifiedUrl = `${BASE_URL}${match[1]}`;
+            }
+            // Check if the message contains a link matching the pw.live pattern
+            else if (regexPwLive.test(msg.text)) {
+                match = msg.text.match(regexPwLive);
+                modifiedUrl = `${BASE_URL}${match[0]}`;
+            }
+            // Check if the message contains a link with v= parameter
+            else if (regexVParam.test(msg.text)) {
+                match = msg.text.match(regexVParam);
+                modifiedUrl = `${BASE_URL}${match[0]}`;
+            }
+
+            // If a match is found, send the modified URL
+            if (modifiedUrl) {
+                bot.sendMessage(chatId, 
+                    ` 
 USE THIS LINK ON 1DM APP AND BOT:
-     
-${modifiedUrl1}
-  
+         
+${modifiedUrl}${PIXEL1},
+
+${modifiedUrl}${PIXEL2},
+
+${modifiedUrl}${PIXEL3},
+
+${modifiedUrl}${PIXEL4},
+      
 powered by @omjibotz
-                `
-            );
-        }
-        // Check if the message contains a link matching the pw.live pattern
-        else if (msg.text && regexPwLive.test(msg.text)) {
-            const match = msg.text.match(regexPwLive);
-            const modifiedUrl2 = `https://penpencilvod.pc.cdn.bitgravity.com/${match[0]}/master.m3u8`;
-            
-            bot.sendMessage(chatId, 
-                ` 
-USE THIS LINK ON 1DM APP AND BOT:
-     
-${modifiedUrl2}
-  
-powered by @omjibotz
-                `
-            );
-        }
-        // If the link does not match either pattern
-        else {
-            bot.sendMessage(chatId, 'Please enter the right link && report wrong link here @Lob_u_omi_bot');
+                    `
+                );
+            } else {
+                // If no match is found, prompt the user to enter the right link
+                bot.sendMessage(chatId, 'Please enter the right link. & report wrong link here @Lob_u_omi_bot');
+            }
         }
     });
 
